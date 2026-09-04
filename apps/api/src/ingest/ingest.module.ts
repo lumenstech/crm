fb0dfc60faf8aa6ca5e60f006c8b935dcf086928
lumenstech <124342884+lumenstech@users.noreply.github.com>
2026-09-04 00:44:27 -0400

    feat(api): wire signal qualification service

--- apps/api/src/ingest/ingest.module.ts
+++ apps/api/src/ingest/ingest.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { CompaniesModule } from "../companies/companies.module";
+import { DealsModule } from "../deals/deals.module";
 import { IngestRouter } from "./ingest.router";
 import { IngestService } from "./ingest.service";
+import { SignalQualificationService } from "./signal-qualification.service";
 
 @Module({
-	imports: [CompaniesModule],
-	providers: [IngestService, IngestRouter],
-	exports: [IngestService],
+	imports: [CompaniesModule, DealsModule],
+	providers: [IngestService, SignalQualificationService, IngestRouter],
+	exports: [IngestService, SignalQualificationService],
 })
 export class IngestModule {}
