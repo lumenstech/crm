284c5f9b79194d61de2daa14a47447c971c9caf3
lumenstech <124342884+lumenstech@users.noreply.github.com>
2026-09-07 14:36:49 -0400

    refactor(api): use one opportunity source scanner

--- apps/api/src/ingest/ingest.module.ts
+++ apps/api/src/ingest/ingest.module.ts
@@ -6,7 +6,6 @@ import { IngestRouter } from "./ingest.router";
 import { IngestService } from "./ingest.service";
 import { OpportunityOpsService } from "./opportunity-ops.service";
 import { OpportunitySourceService } from "./opportunity-source.service";
-import { RegionalOpportunitySourceService } from "./regional-opportunity-source.service";
 import { SignalQualificationService } from "./signal-qualification.service";
 
 @Module({
@@ -16,7 +15,6 @@ import { SignalQualificationService } from "./signal-qualification.service";
 		SignalQualificationService,
 		OpportunityOpsService,
 		OpportunitySourceService,
-		RegionalOpportunitySourceService,
 		GuyanaOpportunityService,
 		IngestRouter,
 	],
@@ -25,7 +23,6 @@ import { SignalQualificationService } from "./signal-qualification.service";
 		SignalQualificationService,
 		OpportunityOpsService,
 		OpportunitySourceService,
-		RegionalOpportunitySourceService,
 		GuyanaOpportunityService,
 	],
 })
